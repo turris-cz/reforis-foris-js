a4267e9b550372c19567630f329d42366a8cc3e9
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2025-02-21 16:41:37 +0100

    Make thead of RichTable light

--- src/common/RichTable/RichTableHeader.js
+++ src/common/RichTable/RichTableHeader.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ * Copyright (C) 2019-2025 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -32,7 +32,7 @@ function RichTableHeader({ table, flexRender }) {
     };
 
     return (
-        <thead className="thead-light">
+        <thead className="table-light">
             {table.getHeaderGroups().map((headerGroup) => (
                 <tr key={headerGroup.id} role="row">
                     {headerGroup.headers.map((header) => (
